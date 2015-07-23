0e54bc56f804979bbdc6884091efe747dc73ff1b
jcarlosmejiaayala <jcarlosmejiaayala@gmail.com>
2015-07-23 10:35:59 -0500

    para produccion

--- server/api/user/user.controller.js
+++ server/api/user/user.controller.js
@@ -23,7 +23,7 @@ exports.index = function(req, res) {
 /**
  * Creates a new user
  */
-exports.create = function (req, res, next) {
+exports.create = function (req, res) {
   var newUser = new User(req.body);
   newUser.provider = 'local';
   newUser.role = 'user';
--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -4,20 +4,21 @@
 // =================================
 module.exports = {
   // Server IP
-  ip:       process.env.OPENSHIFT_NODEJS_IP ||
-            process.env.IP ||
-            undefined,
+  ip: process.env.OPENSHIFT_NODEJS_IP ||
+  process.env.IP ||
+  undefined,
 
   // Server port
-  port:     process.env.OPENSHIFT_NODEJS_PORT ||
-            process.env.PORT ||
-            8080,
+  port: process.env.OPENSHIFT_NODEJS_PORT ||
+  process.env.PORT ||
+  8080,
 
   // MongoDB connection options
   mongo: {
-    uri:    process.env.MONGOLAB_URI ||
-            process.env.MONGOHQ_URL ||
-            process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
-            'mongodb://localhost/bodychallenge'
+    uri: 'mongodb://bodymass:bodymass@dbh35.mongolab.com:27357/bodymass',
+    options: {
+      server: {socketOptions: {keepAlive: 1, connectTimeoutMS: 30000}},
+      replset: {socketOptions: {keepAlive: 1, connectTimeoutMS: 30000}}
+    }
   }
 };
